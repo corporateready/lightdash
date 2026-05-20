ab8e0dacced07c466659503fb7aff5e93fdbea13
Joao Viana <joao@lightdash.com>
2026-05-20 09:17:30 +0100

    Add content table compatibility layer

---
+++ packages/frontend/src/components/common/InHouseTable/index.ts
@@ -0,0 +1,17 @@
+export { InHouseTable, MantineReactTable } from './InHouseTable';
+export { InHouseTableSearchInput } from './InHouseTableSearchInput';
+export { useInHouseTable, useMantineReactTable } from './useInHouseTable';
+export type { InHouseTableSearchInputProps } from './InHouseTableSearchInput';
+export type {
+    InHouseTableColumnDef,
+    InHouseTableInstance,
+    InHouseTableOptions,
+    InHouseTableSortingState,
+    InHouseTableVirtualizer,
+    MRT_ColumnDef,
+    MRT_Row,
+    MRT_SortingState,
+    MRT_TableInstance,
+    MRT_TableOptions,
+    MRT_Virtualizer,
+} from './types';
